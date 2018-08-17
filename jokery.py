c1acbcc29d0915deea06dfac9a6a6f0e62c18f2e
Thejayman77 <thejayman77@gmail.com>
2018-08-17 10:57:00 -0400

    Initial

---
+++ jokery.py
@@ -0,0 +1,19 @@
+import tkinter
+from tkinter import ttk
+from tkinter import messagebox
+
+
+##################
+# Setup GUI object
+##################
+gui = tkinter.Tk()
+gui.title("The jokery")
+gui.geometry("640x480")
+
+
+
+
+#################
+# GUI mainloop()
+#################
+gui.mainloop()
